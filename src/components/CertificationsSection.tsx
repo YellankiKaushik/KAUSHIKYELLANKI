91b492b9d197add7e5e48031400918f1c780dfb0
Kaushik Yellanki <kaushikyellanki@gmail.com>
2026-01-28 21:52:37 +0530

    added new features

--- src/components/CertificationsSection.tsx
+++ src/components/CertificationsSection.tsx
@@ -2,20 +2,79 @@ import { motion } from "framer-motion";
 
 const certifications = [
   {
-    title: "Make Agentic AI Work for You",
-    issuer: "IBM",
-    link: "https://www.credly.com/badges/fecb2723-c3f4-4e1a-813a-76f402ff80ff/public_url",
+    title: "Alteryx Auto Insights Micro-Credential ",
+     issuer: "Alteryx ",
+        link: "https://www.credly.com/badges/4cd6ee80-176b-426c-827f-eb434696c22c/public_url ",
   },
+  
   {
-    title: "Alteryx Designer Core Certification",
-    issuer: "Alteryx",
-    link: "https://www.credly.com/badges/de36773d-5970-4088-9964-4aded97fa808/public_url",
+     title: " Alteryx Designer Core Certification",
+     issuer: "Alteryx ",
+        link: " https://www.credly.com/badges/de36773d-5970-4088-9964-4aded97fa808/public_url ",
   },
-  {
-    title: "Alteryx Auto Insights Micro-Credential",
-    issuer: "Alteryx",
-    link: "https://www.credly.com/badges/20c52c2f-9b0d-44d9-8d5e-1da28e1b2bfd/public_url",
+  
+  {	
+    title: "Alteryx Foundational Micro-Credential ",
+     issuer: "Alteryx ",
+        link: " https://www.credly.com/badges/20c52c2f-9b0d-44d9-8d5e-1da28e1b2bfd/public_url",
   },
+  
+  {	
+    title: "Machine Learning Fundamentals Micro-Credential ",
+     issuer: " Alteryx",
+        link: "https://www.credly.com/badges/9758c6ff-ab67-48e5-83bf-2589da4605b6/public_url ",
+  },
+  
+  {	
+    title: "Make Agentic AI Work for You ",
+     issuer: " IBM SkillsBuild ",
+        link: " https://www.credly.com/badges/fecb2723-c3f4-4e1a-813a-76f402ff80ff/public_url",
+  },
+  
+  {	
+    title: "AWS Academy Graduate - Cloud Architecting - Training Badge",
+     issuer: " Amazon Web Services Training and Certification",
+        link: "https://www.credly.com/badges/1e618cad-5474-4023-8cca-2c4e61d99c04/public_url ",
+  },
+     
+  {	
+    title: "AWS Academy Graduate - Machine Learning Foundations - Training Badge ",
+     issuer: " Amazon Web Services Training and Certification",
+        link: " https://www.credly.com/badges/0f908261-53e5-4532-9892-0d92a0992081/public_url",
+  },
+     
+  
+  {	
+    title: "CCNA: Enterprise Networking, Security, and Automation ",
+     issuer: "Cisco ",
+        link: "https://www.credly.com/badges/15f81fc6-b7a7-40da-be09-6e4e42a1a67c/public_url ",
+  },
+     
+  
+  {	
+    title: " CCNA: Switching, Routing, and Wireless Essentials",
+     issuer: "Cisco ",
+        link: " https://www.credly.com/badges/e5ce3b74-89e1-47e8-bce9-68c0906571e0/public_url",
+  },
+  
+  {	
+    title: " Machine Learning for All ",
+     issuer: "University of London ",
+        link: "https://coursera.org/share/0e5760685e18a9a003ee4b8c8251e1a7 ",
+  },
+  {	
+    title: "Tata - Data Visualisation: Empowering Business with Effective Insights Job Simulation ",
+     issuer: "Forage ",
+        link: "https://www.theforage.com/completion-certificates/ifobHAoMjQs9s6bKS/MyXvBcppsW2FkNYCX_ifobHAoMjQs9s6bKS_m3xGpZ3r6inch3q7C_1739026179981_completion_certificate.pdf ",
+  },
+  
+  {	
+    title: " Deloitte Australia - Data Analytics Job Simulation",
+     issuer: "Forage ",
+        link: " https://www.theforage.com/completion-certificates/9PBTqmSxAf6zZTseP/io9DzWKe3PTsiS6GG_9PBTqmSxAf6zZTseP_m3xGpZ3r6inch3q7C_1738957277947_completion_certificate.pdf ",
+  },
+  
+  
 ];
 
 const CertificationsSection = () => {
